98b392abc393c58a3ad5a3f80cd20bdea2f68060
Antu976 <137787227+Antu976@users.noreply.github.com>
2023-12-18 15:14:11 +0100

    Adrien

--- view/view_article.php
+++ view/view_article.php
@@ -2,7 +2,7 @@
 
 <form action="index.php" method="POST">
         <input type="text" name="nom_article" placeholder="nom de l'article">
-        <input type="number" step="0.01" name="Prix_article" placeholder="Prix de l'article">
+        <input type="number" step="0.01" name="prix_article" placeholder="prix de l'article">
         <input type="submit" name="submit">
 </form>
 
